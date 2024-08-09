1ad7827969db63ef0e906077b36bec09a64a031c
Tomás <iglesiastomas99@gmail.com>
2024-08-09 20:38:13 -0300

    add service

---
+++ dashboard/src/main/java/com/trackwise/dashboard/DashboardService.java
@@ -0,0 +1,10 @@
+package com.trackwise.dashboard;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardService {
+    public void getDashboard() {
+        System.out.println("Getting dashboard");
+    }
+}
