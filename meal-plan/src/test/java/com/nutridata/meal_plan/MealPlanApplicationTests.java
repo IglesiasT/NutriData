0eba3f5da740954e965caae7d589985a74f67760
Tomás <iglesiastomas99@gmail.com>
2024-10-18 09:36:30 -0300

    add meal plan microservice

---
+++ meal-plan/src/test/java/com/nutridata/meal_plan/MealPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutridata.meal_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
