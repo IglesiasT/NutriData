f10be10d0e37fed1e31efc53c41651f2a45c4549
Tomás <iglesiastomas99@gmail.com>
2025-03-03 16:51:46 -0300

    create PatientDTO

---
+++ patient/src/main/java/com/nutridata/patient/PatientDTO.java
@@ -0,0 +1,74 @@
+package com.nutridata.patient;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class PatientDTO {
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String surname;
+
+    @Email
+    private String email;
+
+    @Min(1)
+    private Integer age;
+
+    @Min(0)
+    private Double weight;
+
+    @Min(0)
+    private Double height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+}
+
